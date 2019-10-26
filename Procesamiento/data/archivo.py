9b778021c5c196c13ce6ec853262ce518cc23815
Enmanuel Magallanes <emmanu2006@hotmail.com>
2019-10-25 22:10:40 -0500

    implementacion de pequenios algoritmos de limpieza para los comentarios y organizacion de los mismos en diccionarios

--- Procesamiento/data/archivo.py
+++ Procesamiento/data/archivo.py
@@ -1,1 +1,42 @@
-archivo = file();
+import funciones as fn
+import os
+
+rutaParcial = "../detalle/"
+dicc = {}
+
+print("Cargando y limpiando comentarios. . .")
+for nombreArchivo in os.listdir(rutaParcial):
+
+    rutaCompleta = os.path.join(rutaParcial, nombreArchivo)
+    archivo = open(rutaCompleta, "r", encoding='UTF8')
+    nombreArchivo = nombreArchivo.replace(".txt", "")
+
+    indTermino = nombreArchivo.find("1S") if nombreArchivo.find("1S") != -1 else nombreArchivo.find("2S")
+
+    termino = nombreArchivo[indTermino::]
+    anio = nombreArchivo[indTermino-4:indTermino]
+    codigoMat = nombreArchivo[:indTermino-4]
+
+    profesor = archivo.readline().replace(",", " ").replace("\n", "")
+
+    #Linea para salta los especios para llegar al primer comentario
+    archivo.readline()
+    archivo.readline()
+
+    comentarios = archivo.readlines()
+    comentsLimpios = []
+
+    for comentario in comentarios:
+
+        if len(comentario) > 1:
+            # 1.Filto las Stopwords
+            comentFiltrado = fn.limpiarStopWords(comentario)
+            # 2.Sepracion de palabras
+            comentsLimpios.append(fn.separarPalabras(comentFiltrado))
+
+    dicc = fn.llenarDiccionario(profesor, codigoMat, termino, anio, comentsLimpios, dicc)
+
+print("ROCÍO ELIZABETH MERA SUÁREZ: " +str(dicc.get("ROCÍO ELIZABETH MERA SUÁREZ")))
+
+#Tambien se puede simplicar palabras como muuuuy a muy
+#sin embargo creo que esto si es significativo para nuestro estudio
---
+++ Procesamiento/data/funciones.py
@@ -0,0 +1,35 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
+
+#Coloco aqui esta linea para que no se ejecute cada vez que se llama a la funcion
+stop_words = set(stopwords.words('spanish'))
+
+def limpiarStopWords(comentario):
+
+    comentario = comentario.replace("\n", "")
+    oraciones = word_tokenize(comentario)
+    comentFiltrado = [w for w in oraciones if not w in stop_words]
+    return " ".join(comentFiltrado)
+
+def separarPalabras(comentFiltrado):
+
+    return " ".join(re.findall("[A-Z][^A-Z]*", comentFiltrado))
+
+def llenarDiccionario(profesor, codigoMat, termino, anio, comentsLimpios, dicc):
+
+    if profesor not in dicc.keys():
+        dicc[profesor] = {codigoMat: {anio: {termino: comentsLimpios}}}
+    else:
+        if codigoMat not in dicc[profesor].keys():
+            dicc[profesor][codigoMat] = {anio: {termino: comentsLimpios}}
+        else:
+            if anio not in dicc[profesor][codigoMat].keys():
+                dicc[profesor][codigoMat][anio] = {termino: comentsLimpios}
+            else:
+                if termino not in dicc[profesor][codigoMat][anio].keys():
+                    dicc[profesor][codigoMat][anio][termino] = comentsLimpios
+                else:
+                    dicc[profesor][codigoMat][anio][termino].extend(comentsLimpios)
+
+    return dicc
